012a9665a32bf5e829554664a7894f574c9da09d
wohaidingdezhu <1054530015@qq.com>
2022-10-11 23:10:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const chokidar = require("chokidar");
+const { exec, spawn } = require("child_process");
+let childProcess;
+let debounceRestart = debounce(restart, 500);
+
+// One-liner for current directory
+chokidar.watch(["main.js"]).on("all", (event, path) => {
+  console.log(event, path);
+
+  debounceRestart();
+});
+
+function restart() {
+  console.log("restart");
+  childProcess && childProcess.kill();
+
+  childProcess = spawn("node", ["main.js"], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+}
+
+function debounce(fn, delay) {
+  let id;
+  return () => {
+    clearTimeout(id);
+
+    id = setTimeout(() => {
+      fn();
+    }, delay);
+  };
+}
+
+// exec("node test.js", (err, stdout) => {
+//   console.log(stdout);
+// });
+
+// spawn("node", ["test.js"], {
+//   stdio: [process.stdin, process.stdout, process.stderr],
+// });
---
+++ main.js
@@ -0,0 +1,15 @@
+const Koa = require("koa");
+const app = new Koa();
+app.use((ctx) => {
+  ctx.body = "hi my name is cuixiaorui111222222221222233333";
+});
+
+app.listen(3000);
+// nodemon
+// 1. 当代码改变的时候
+//   - fs.watch()
+//   - chokidar
+
+// 2. 重新的启动服务(kill)
+//   - node main.js -> command
+//   - nodejs -> exec  || spawn
